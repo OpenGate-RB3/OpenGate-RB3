39d4465b6bf042e6e07b10e9558ccf6b756c2e56
Daniil Katulevskiy <dankatul@gmail.com>
2025-05-16 12:55:25 -0700

    update install script

--- INSTALL.bash
+++ INSTALL.bash
@@ -3,13 +3,17 @@
 # Define URLs for the files to download
 MEDIAMTX_URL="https://github.com/bluenviron/mediamtx/releases/download/v1.12.2/mediamtx_v1.12.2_linux_arm64.tar.gz"
 YML_URL="https://github.com/OpenGate-RB3/OpenGate-RB3/blob/main/board-configs/mediamtx.yml?raw=true"
+START_AUDIO_URL="https://raw.githubusercontent.com/OpenGate-RB3/OpenGate-RB3/refs/heads/main/board-configs/start_audio.bash"
+START_VIDEO_URL="https://raw.githubusercontent.com/OpenGate-RB3/OpenGate-RB3/refs/heads/main/board-configs/start_video.bash"
 
 # Define paths
 DOWNLOAD_DIR="/tmp"
 INSTALL_DIR="/usr/local/bin"
 TAR_FILE="$DOWNLOAD_DIR/mediamtx_v1.12.2_linux_arm64.tar.gz"
 YML_FILE="$DOWNLOAD_DIR/mediamtx.yml"
-EXTRACT_DIR="$DOWNLOAD_DIR/mediamtx_v1.12.2_linux_arm64"
+START_AUDIO_FILE="$DOWNLOAD_DIR/start_audio.bash"
+START_VIDEO_FILE="$DOWNLOAD_DIR/start_video.bash"
+EXTRACT_DIR="$DOWNLOAD_DIR"
 
 # Download mediamtx tar.gz file
 echo "Downloading mediamtx tar file..."
@@ -25,15 +29,36 @@ mv "$EXTRACT_DIR/mediamtx" $INSTALL_DIR/
 
 # Clean up extracted files
 echo "Cleaning up..."
-rm -rf $EXTRACT_DIR
+rm $DOWNLOAD_DIR/LICENSE $DOWNLOAD_DIR/mediamtx_v1.12.2_linux_arm64.tar.gz
 
 # Download the mediamtx.yml file
 echo "Downloading mediamtx.yml..."
 wget $YML_URL -O $YML_FILE
 
-# Move the mediamtx.yml file to /usr/local/bin/
-echo "Moving mediamtx.yml to /usr/local/bin..."
-mv $YML_FILE $INSTALL_DIR/
+# Download the start_audio.bash file
+echo "Downloading start_audio.bash..."
+wget $START_AUDIO_URL -O $START_AUDIO_FILE
+
+# Download the start_video.bash file
+echo "Downloading start_video.bash..."
+wget $START_VIDEO_URL -O $START_VIDEO_FILE
+
+# Get the IP address of the system
+ipaddr=$(hostname -i | awk '{print $1}')
+
+# Use sed to replace ${IPHOSTNAME} with the system's IP address in all the downloaded files
+echo "Replacing IPHOSTNAME in files..."
+
+sed -i "s|\${IPHOSTNAME}|$ipaddr|g" $YML_FILE
+sed -i "s|\${IPHOSTNAME}|$ipaddr|g" $START_AUDIO_FILE
+sed -i "s|\${IPHOSTNAME}|$ipaddr|g" $START_VIDEO_FILE
+
+# Move the files to /usr/local/bin/
+echo "Moving files to /usr/local/etc..."
+mv $YML_FILE /usr/local/etc/
+echo "Moving files to /usr/local/bin..."
+mv $START_AUDIO_FILE $INSTALL_DIR/
+mv $START_VIDEO_FILE $INSTALL_DIR/
 
 # Script complete
 echo "Installation complete."
