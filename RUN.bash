d29ecb141f4816a029fd32d5f627402232ccc975
Andrew Pegg <54915639+andrewcomputsci2019@users.noreply.github.com>
2025-06-04 10:14:10 -0700

    Update RUN.bash
    added mediamtx to run script

--- RUN.bash
+++ RUN.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# start mediamtx server
+nohup mediamtx >/dev/null 2>&1 &
+MEDIAMTX_PID=$!
+
 # Start the video and audio scripts in the background
 nohup start_video.bash >/dev/null 2>&1 &
 VIDEO_PID=$!
@@ -7,9 +11,12 @@ VIDEO_PID=$!
 nohup start_audio.bash >/dev/null 2>&1 &
 AUDIO_PID=$!
 
+
+
 # Trap SIGINT (Ctrl-C) to kill the background processes
-trap "kill $VIDEO_PID $AUDIO_PID" SIGINT
+trap "kill $VIDEO_PID $AUDIO_PID $MEDIAMTX_PID" SIGINT
 
 # Wait for the background processes to finish
 wait $VIDEO_PID
 wait $AUDIO_PID
+wait $MEDIAMTX_PID
